1572fc898fd2854c85f303e75c515368f566ab92
will2dev <wylliam.bastiani@gmail.com>
2017-11-23 22:49:04 -0200

    pushing column name change

--- src/scripts/mssql/select_all_database_foreign_keys.sql
+++ src/scripts/mssql/select_all_database_foreign_keys.sql
@@ -4,7 +4,7 @@ SELECT
 	,FK.[type] AS [constraint_type]
 	,FK.parent_object_id AS [parent_table_id]
 	,FKC.parent_column_id AS [parent_column_id]
-	,FK.referenced_object_id AS [referenced_object_id]
+	,FK.referenced_object_id AS [referenced_table_id]
 	,FKC.referenced_column_id AS [referenced_column_id]
 FROM sys.foreign_keys AS FK
 	INNER JOIN sys.foreign_key_columns AS FKC ON FKC.constraint_object_id = FK.object_id;
